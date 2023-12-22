61b9f996cdad94f95a74a80fbf5d82c7a6657f47
Foysal Rahman <fr25foysal@gmail.com>
2023-12-22 19:56:26 +0600

    vercel hosted

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const taskCollection = client.db('manageme').collection('tasks')
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     app.post('/tasks',async(req,res)=>{
       const task = req.body
